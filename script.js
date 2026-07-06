7bae02498e1946a2512fb35b787042a193fe59bf
Eliasd <liakosdel2012@gmail.com>
2026-07-07 00:23:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ button.addEventListener('click', function() {
     window.open('https://discordapp.com/users/1130487418244579350', '_blank');
     
 });
+
+
+
+const coolMusic = document.getElementById('music');
+const soundButton = document.getElementById('sound-button')
+const volumeSlider = document.getElementById('volume-slider')
+let coolMusicPlaying = false;
+soundButton.addEventListener('click', () => {
+    if (coolMusicPlaying) {
+        coolMusic.pause();
+        soundButton.textContent = 'Play Music';
+        volumeSlider.classList.remove('visible')
+    } else {
+        coolMusic.play();
+        soundButton.textContent = 'Stop Music';
+        volumeSlider.classList.add('visible');
+    }
+    coolMusicPlaying = !coolMusicPlaying;
+});
+
+volumeSlider.addEventListener('input', () => { 
+    coolMusic.volume = volumeSlider.value;
+});
